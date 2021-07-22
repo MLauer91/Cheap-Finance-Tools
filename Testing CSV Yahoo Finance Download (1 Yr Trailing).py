3f78c0f467586e3e262d16ac4fca1048fef6238f
Tong <butong369@gmail.com>
2021-07-22 05:01:01 -0500

    More basic way
    Hello MLauer91, I come cross this repo today when yfinance isn't working for me. I like the way you use basic yahoo query url to get data. I have put together a more basic way, let me know what you think.

--- Testing CSV Yahoo Finance Download (1 Yr Trailing).py
+++ Testing CSV Yahoo Finance Download (1 Yr Trailing).py
@@ -1,44 +1,21 @@
 import pandas as pd
-import io
-import requests
-import datetime
-from tabulate import tabulate
+from datetime import datetime
 
-ticker = input('Ticker: ').upper()
+tickers = ['AAPL', 'AAL']
+start_date = int(datetime(2015, 1, 1).timestamp())-1
+end_date = int(datetime(2016, 1, 1).timestamp())+1
 
 # ---------------- YAHOO CALCULATIONS ----------------
-while ticker != 'QUIT':
-    # ticker = 'SPY'
-    refmultiply = 86400
-    refdateyahoo = 1420156800  # This is 1/2/2015
-    refdate = datetime.date(2015, 1, 2)
-    startdate = datetime.date(datetime.date.today().year - 1, datetime.date.today().month, datetime.date.today().day)
-    enddate = datetime.date.today()
-    enddatetoday = datetime.date(datetime.date.today().year, datetime.date.today().month + 1, datetime.date.today().day)
 
-    if refdate == startdate:
-        startdateyahoo = refdateyahoo
-    else:
-        startdateyahoo = refdateyahoo + (startdate - refdate).days * refmultiply
+def yahoo_download(tickers, start, end ,OHLC):
+    df = pd.DataFrame()
+    for ticker in tickers:
+        url = 'https://query1.finance.yahoo.com/v7/finance/download/' + ticker + \
+            '?period1=' + str(start) + '&period2=' + str(end) + '&interval=1d&events=history'
+        price = pd.read_csv(url)
+        df[ticker] = price[OHLC]
+    df['Date'] = price['Date']
+    return df
 
-    if enddate == refdate:
-        enddateyahoo = refdateyahoo
-    elif enddate == datetime.date.today():
-        enddateyahoo = refdateyahoo + (enddatetoday - refdate).days * refmultiply
-    else:
-        enddateyahoo = refdateyahoo + (enddate - refdate).days * refmultiply
-
-    url = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + \
-          "?period1=" + str(startdateyahoo) + "&period2=" + str(enddateyahoo) + "&interval=1d&events=history"
-    s = requests.get(url).content
-    fullLoad = pd.read_csv(io.StringIO(s.decode('utf-8')))
-    fullLoad['Date'] = pd.to_datetime(fullLoad['Date'])
-    fullLoad = fullLoad.set_index('Date')
-
-    # ------------ DISPLAYING RESULTS -------------
-    headers = list(fullLoad.columns.values)
-    print(tabulate(fullLoad.sort_index(ascending=False), headers, tablefmt="simple"))
-
-    ticker = input('Ticker: ').upper()
-
-print('Complete')
+df = yahoo_download(tickers, start_date, end_date, 'Adj Close')
+df.head()
